b7987c43657f5bb2bbcd380c84eb33cda364d3fd
Vishnu P <vinventor1999@gmail.com>
2024-07-15 19:30:22 +0530

    Videos 1,2 and 3 completed

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>WEB DEVELOPMENT</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+      <h1>WEB DEVELOPMENT</h1>
+  )
+}
+
+export default App
